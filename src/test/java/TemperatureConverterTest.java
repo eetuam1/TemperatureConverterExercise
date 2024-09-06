f55f6ef07ecf4893581b6475517741e9656953dd
Eetu <ediz.mantyla@hotmail.com>
2024-09-06 10:56:35 +0300

    sfd

--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -12,33 +12,23 @@ public class TemperatureConverterTest {
 
     @Test
     public void testFahrenheitToCelsius() {
-        // Common case
         assertEquals(0.0, converter.fahrenheitToCelsius(32), 0.001);
-        // Boiling point of water
         assertEquals(100.0, converter.fahrenheitToCelsius(212), 0.001);
-        // Extreme case
         assertEquals(-40.0, converter.fahrenheitToCelsius(-40), 0.001);
     }
 
     @Test
     public void testCelsiusToFahrenheit() {
-        // Common case
         assertEquals(32.0, converter.celsiusToFahrenheit(0), 0.001);
-        // Boiling point of water
         assertEquals(212.0, converter.celsiusToFahrenheit(100), 0.001);
-        // Extreme case
         assertEquals(-40.0, converter.celsiusToFahrenheit(-40), 0.001);
     }
 
     @Test
     public void testIsExtremeTemperature() {
-        // Extremely low temperature
         assertTrue(converter.isExtremeTemperature(-50));
-        // Extremely high temperature
         assertTrue(converter.isExtremeTemperature(60));
-        // Normal temperature
         assertFalse(converter.isExtremeTemperature(20));
-        // Another normal temperature
         assertFalse(converter.isExtremeTemperature(-10));
     }
 
